73c12755fb8fbaed82697a045ec4e142296f0808
oliveriofogiel <oliifogiel@gmail.com>
2026-03-24 15:07:02 -0300

    Fuel Mechanic Implement Test
    Train now has fuel

--- Assets/Scripts/LocomotiveMovement.cs
+++ Assets/Scripts/LocomotiveMovement.cs
@@ -1,23 +1,49 @@
 using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class LocomotiveMovement : MonoBehaviour, IWagon
 {
-    [SerializeField] private float baseSpeed;
+    //[SerializeField] private float baseSpeed;
     [SerializeField] private SharedData speedData;
+
+    [SerializeField] private float maxFuel = 100f;
+    [SerializeField] private float currentFuel = 100f;
+    [SerializeField] private float fuelUseXSecond = 5f;
+    [SerializeField] private Vector3 moveDirection = Vector3.right;
+    [SerializeField] private float baseSpeed = 5f;
+    [SerializeField] private float debugFuelAmount = 20f;
+    public float CurrentFuel => currentFuel;
+    public float MaxFuel => maxFuel;
+    public bool HasFuel => currentFuel > 0f;
 
     void Start()
     {
-        speedData.speed = baseSpeed;
+        currentFuel = Mathf.Clamp(currentFuel, 0f, maxFuel);
+        //speedData.speed = baseSpeed;
+        UpdateSharedSpeed();
+
     }
 
     void Update()
     {
-
+        HandleDebugFuel();
+        Move();
     }
 
     void Move()
     {
+        if (!HasFuel) 
+        {
+            speedData.speed = 0f;
+            return;
+        }
+        speedData.speed = baseSpeed;
+        transform.position += moveDirection.normalized * speedData.speed * Time.deltaTime;
+        ConsumeFuel(fuelUseXSecond * Time.deltaTime);
+
+        
     }
 
     void Interact()
@@ -25,8 +51,54 @@ public class LocomotiveMovement : MonoBehaviour, IWagon
 
     }
 
-    void ChangeSpeed(float additive)
+    /*void ChangeSpeed(float additive)
     {
-        speedData.speed += additive;
+        baseSpeed = Mathf.Max(0f, baseSpeed + additive);
+        //speedData.speed += additive;
+        UpdateSharedSpeed();
+    }*/
+
+    public void AddFuel(float amount)
+    {
+        currentFuel = Mathf.Clamp(currentFuel + amount, amount, maxFuel);
+        UpdateSharedSpeed();
+        Debug.Log("Current Fuel: " + currentFuel);
+    }
+
+    public void RemoveFuel(float amount) //llamar a esta funcion x si hay alguien o algo que te reste nafta 
+    {
+        currentFuel = Mathf.Clamp(currentFuel - amount, 0f, maxFuel);
+        UpdateSharedSpeed();
+        Debug.Log("Current Fuel: " + currentFuel);
+    }
+
+    private void ConsumeFuel(float amount) //gasto natural del tren
+    {
+        currentFuel = Mathf.Clamp(currentFuel - amount, 0f, maxFuel);
+        UpdateSharedSpeed();
+    }
+
+    private void UpdateSharedSpeed()
+    {
+        if (HasFuel) 
+        {
+            speedData.speed = baseSpeed;
+        }
+        else
+        {
+            speedData.speed = 0f;
+        }
+    }
+
+    private void HandleDebugFuel()
+    {
+        if (Keyboard.current.rKey.wasPressedThisFrame)
+        {
+            AddFuel(debugFuelAmount);
+        }
+        if (Keyboard.current.tKey.wasPressedThisFrame)
+        {
+            RemoveFuel(debugFuelAmount);
+        }
     }
 }
