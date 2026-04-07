ebf2b8552737b95003c489fcecc881741dadcd13
oliveriofogiel <oliifogiel@gmail.com>
2026-04-07 12:00:56 -0300

    Shop UI, Scene created

---
+++ Assets/Scripts/UI/UIShopManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class UIShopManager : MonoBehaviour
+{
+    public void OnWagonUpgrade1()
+    {
+        Debug.Log("Wagon 1 upgrade button clicked");
+    }
+    public void OnWagonUpgrade2()
+    {
+        Debug.Log("Wagon 2 upgrade button clicked");
+    }
+    public void OnWagonUpgrade3()
+    {
+        Debug.Log("Wagon 3 upgrade button clicked");
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+}
