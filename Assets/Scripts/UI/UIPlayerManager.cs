77031ea0c0a68a82a6d729123dabf59759a96683
laureanoan0 <laureanomeyer22@gmail.com>
2026-05-19 17:14:34 -0300

    Ui rework FML

--- Assets/Scripts/UI/UIPlayerManager.cs
+++ Assets/Scripts/UI/UIPlayerManager.cs
@@ -1,28 +1,36 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
+using UnityEngine.UIElements;
 
 public class UIPlayerManager : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private PlayerBrain player;
+    [SerializeField] private UIDocument uiDocument;
 
 
     [Header("Locomotive UI")]
+<<<<<<< Updated upstream
     [SerializeField] private Image fuelFillImage;
     [SerializeField] private Image fuelMaxCapacityImage;
     [SerializeField] private Image shieldImage;
     [SerializeField] private Image backgroundImage;
     [SerializeField] private Image consumedImage;
+=======
+    [SerializeField] private GameObject fuelIndicator;
+    [SerializeField] private GameObject fuelMaxCapacityIndicator;
+    [SerializeField] private UnityEngine.UI.Image shieldImage;
+>>>>>>> Stashed changes
 
     [Header("Inventory UI")]
-    [SerializeField] private Image coalImage;
-    [SerializeField] private Image goldImage;
+    [SerializeField] private UnityEngine.UI.Image coalImage;
+    [SerializeField] private UnityEngine.UI.Image goldImage;
     [SerializeField] private TMP_Text goldText;
 
     [Header("Run Progress")]
     [SerializeField] private SceneRunController sceneRunController;
-    [SerializeField] private Image runProgressFill;
+    [SerializeField] private UnityEngine.UI.Image runProgressFill;
     [SerializeField] private RectTransform trainIcon;
     [SerializeField] private RectTransform startPoint;
     [SerializeField] private RectTransform endPoint;
@@ -32,13 +40,27 @@ public class UIPlayerManager : MonoBehaviour
     [SerializeField] private GameObject InteractImage;
 
     private Color originalColor;
+<<<<<<< Updated upstream
+=======
+    private LocomotiveBrain locomotive;
+    private VisualElement manecilla;
+    private VisualElement hebilla;
+>>>>>>> Stashed changes
 
 
     private void Start()
     {
         UpdateGoldUI(0);
         InteractImage.SetActive(false);
+<<<<<<< Updated upstream
         consumedImage.gameObject.SetActive(false);
+=======
+        locomotive = RunManager.Instance.LocomotiveBrain;
+
+        var root = uiDocument.rootVisualElement;
+        manecilla = root.Q<VisualElement>("Manecilla");
+        hebilla = root.Q<VisualElement>("Hebilla");
+>>>>>>> Stashed changes
     }
 
     void Update()
@@ -56,6 +78,7 @@ public class UIPlayerManager : MonoBehaviour
 
         if (locomotive != null && locomotive.fuelController != null)
         {
+<<<<<<< Updated upstream
             var currentFill = locomotive.fuelController.CurrentFuel / locomotive.fuelController.FuelMaxCapaciy;
             fuelFillImage.fillAmount = currentFill;
             shieldImage.fillAmount = locomotive.fuelController.CurrentShield / locomotive.fuelController.MaxShield;
@@ -75,7 +98,26 @@ public class UIPlayerManager : MonoBehaviour
                 consumedImage.gameObject.SetActive(false);
             }
 
+=======
+            var currentFuel = locomotive.fuelController.CurrentFuel
+                            / locomotive.fuelController.FuelMaxCapaciy;
+            var currentCapacity = locomotive.fuelController.CurrentMaxFuel
+                                / locomotive.fuelController.FuelMaxCapaciy;
+
+            SetRotation(manecilla, Mathf.Lerp(-90, 80, currentFuel));
+            SetRotation(hebilla, Mathf.Lerp(-90, 80, currentCapacity));
+
+            // shieldImage sigue igual por ahora
+            shieldImage.fillAmount = locomotive.fuelController.CurrentShield
+                                   / locomotive.fuelController.MaxShield;
+>>>>>>> Stashed changes
         }
+    }
+
+    void SetRotation(VisualElement el, float degrees)
+    {
+        if (el == null) return;
+        el.style.rotate = new StyleRotate(new Angle(degrees, AngleUnit.Degree));
     }
 
     void UpdateInventoryUI()
