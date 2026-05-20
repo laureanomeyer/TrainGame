efea87d463f58f37160b1e0ee1ee4f02386a78bf
Juan-Majork <juani120805@gmail.com>
2026-05-20 20:18:07 -0300

    Fixed tutorial button

--- Assets/Scripts/UI/MainMenu/MainMenuButtons.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuButtons.cs
@@ -4,13 +4,16 @@ using UnityEngine.UI;
 public class MainMenuButtons : MonoBehaviour
 {
     [SerializeField] private Button startButton;
+    [SerializeField] private Button tutorialButton;
     private void Start()
     {
         startButton.onClick.AddListener(GameManager.Instance.StartNewSession);
+        tutorialButton.onClick.AddListener(GameManager.Instance.GoToTutorial);
     }
 
     private void OnDestroy()
     {
         startButton.onClick.RemoveAllListeners();
+        tutorialButton.onClick.RemoveAllListeners();
     }
 }
