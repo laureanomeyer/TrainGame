d7d085227d6ad089e6c5a01f2a4455d72fa01daf
oliveriofogiel <oliifogiel@gmail.com>
2026-06-22 16:43:52 -0300

    Smooth movement del player, estan para testear las variables que son aceleration y desaseleracion en playermovement controller

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -5,6 +5,8 @@ public class PlayerMovementController
 {
     [Header("Movement")]
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float acceleration = 50f;
+    [SerializeField] private float deceleration = 80f;
 
     private Rigidbody rb;
     private LookObjectToMouse lookToMouseController;
@@ -52,10 +54,35 @@ public class PlayerMovementController
 
     private void MovePlayer()
     {
-        Vector3 movement = new Vector3(moveInput.x, 0f, moveInput.y).normalized * speed;
-        movement.y = rb.linearVelocity.y;
-        rb.linearVelocity = movement;
+        Vector3 inputDirection = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
+        Vector3 targetVelocity = inputDirection * speed;
+
+        Vector3 currentHorizontalVelocity = new Vector3(
+            rb.linearVelocity.x,
+            0f,
+            rb.linearVelocity.z
+        );
+
+        float currentAcceleration = inputDirection.sqrMagnitude > 0.001f
+            ? acceleration //lo dejo comentado porque esta medio raro jajaja pero si inputDirection tiene dirección, osea estás apretando W, usa acceleration.
+            : deceleration; //Si inputDirection es Vector3.zero, porque soltaste WASD, usa deceleration
+
+        Vector3 smoothHorizontalVelocity = Vector3.MoveTowards(
+            currentHorizontalVelocity,
+            targetVelocity,
+            currentAcceleration * Time.fixedDeltaTime
+        );
+
+        rb.linearVelocity = new Vector3(
+            smoothHorizontalVelocity.x,
+            rb.linearVelocity.y,
+            smoothHorizontalVelocity.z
+        );
     }
+
+    /*Vector3 movement = new Vector3(moveInput.x, 0f, moveInput.y).normalized * speed;
+    movement.y = rb.linearVelocity.y;
+    rb.linearVelocity = movement;*/
 
     private void RotateToMouse()
     {
