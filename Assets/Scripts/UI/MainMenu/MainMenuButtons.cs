dd70c325d9e1df2669dc60ed27d1b49dd8b0d20b
laureanoan0 <laureanomeyer22@gmail.com>
2026-06-30 15:50:46 -0300

    added waypoint arrows, added quit to main menu

--- Assets/Scripts/UI/MainMenu/MainMenuButtons.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuButtons.cs
@@ -4,16 +4,24 @@ using UnityEngine.UI;
 public class MainMenuButtons : MonoBehaviour
 {
     [SerializeField] private Button startButton;
+    [SerializeField] private Button quitButton;
     //[SerializeField] private Button tutorialButton;
     private void Start()
     {
         startButton.onClick.AddListener(GameManager.Instance.GoToTutorial);
+        quitButton.onClick.AddListener(Quit);
         //tutorialButton.onClick.AddListener(GameManager.Instance.GoToTutorial);
     }
 
     private void OnDestroy()
     {
         startButton.onClick.RemoveAllListeners();
+        quitButton.onClick.RemoveAllListeners();
         //tutorialButton.onClick.RemoveAllListeners();
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
     }
 }
