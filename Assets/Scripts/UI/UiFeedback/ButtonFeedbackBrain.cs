f8e2d5aceec6dcf86782ede85aab911583e4e1e7
laureanoan0 <laureanomeyer22@gmail.com>
2026-07-07 19:44:59 -0300

    Button Animations

---
+++ Assets/Scripts/UI/UiFeedback/ButtonFeedbackBrain.cs
@@ -0,0 +1,56 @@
+using DG.Tweening;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonFeedbackBrain : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Button button;
+    [SerializeField] private float animTime = 0.25f;
+
+    private void OnEnable()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(PlayPressSound);
+    }
+    private void OnDestroy()
+    {
+        button.onClick.RemoveAllListeners();
+    }
+
+    public void OnPointerEnter(PointerEventData data)
+    {
+        PlayHoverSound();
+        PlayHoverAnimation();
+    }
+    public void OnPointerExit(PointerEventData data)
+    {
+        ReverseHoverAnimation();
+    }
+    private void PlayHoverSound()
+    {
+        if (button != null) 
+        {
+            AudioManager.Instance.Play("ButtonHoverSound", 0);
+        }
+    }
+    private void PlayPressSound()
+    {
+        if (button != null)
+        {
+            AudioManager.Instance.Play("ButtonPressedSound", 0);
+            ReverseHoverAnimation();
+        }
+    }
+
+    private void PlayHoverAnimation()
+    {
+        button.transform.DOScale(1.1f, animTime).SetEase(Ease.OutBounce);
+    }
+    private void ReverseHoverAnimation()
+    {
+        button.transform.DOScale(1f, animTime).SetEase(Ease.OutBounce);
+    }
+}
