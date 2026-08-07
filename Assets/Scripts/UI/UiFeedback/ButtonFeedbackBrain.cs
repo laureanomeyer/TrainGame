f4d0c6ce5d4e3a82373e762870317e01c017aa9d
laureanomeyer <laureanomeyer22@gmail.com>
2026-08-07 14:23:05 -0300

    Added close button

--- Assets/Scripts/UI/UiFeedback/ButtonFeedbackBrain.cs
+++ Assets/Scripts/UI/UiFeedback/ButtonFeedbackBrain.cs
@@ -12,13 +12,17 @@ public class ButtonFeedbackBrain : MonoBehaviour, IPointerEnterHandler, IPointer
     private bool isPressed;
     [SerializeField] private float animTime = 0.25f;
     [SerializeField, Range (1f, 1.2f)] private float size = 1.1f;
+    [SerializeField, Range (0.1f, 1f)] private float smallSize = 0.87f;
     [SerializeField] private Ease ease = Ease.OutQuart;
+    private Vector3 initialScale;
 
     private void OnEnable()
     {
         button = GetComponent<Button>();
         isPressed = false;
         button.onClick.AddListener(PlayPressSound);
+
+        initialScale = transform.localScale;
     }
     private void OnDestroy()
     {
@@ -26,25 +30,30 @@ public class ButtonFeedbackBrain : MonoBehaviour, IPointerEnterHandler, IPointer
     }
     public void OnPointerEnter(PointerEventData data)
     {
+        if (button.interactable == false) return;
         if (isPressed) return;
         PlayHoverSound();
         PlayHoverAnimation();
     }
     public void OnPointerExit(PointerEventData data)
     {
+        if (button.interactable == false) return;
         ReverseHoverAnimation(animTime);
     }
     public void OnPointerDown(PointerEventData data) 
     {
+        if (button.interactable == false) return;
         isPressed = true;
         PlayPressedAnimation(animTime);
     }
     public void OnPointerUp(PointerEventData data)
     {
+        if (button.interactable == false) return;
         isPressed = false;
     }
     private void PlayHoverSound()
     {
+        if (button.interactable == false) return;
         if (button != null) 
         {
             AudioManager.Instance.Play("ButtonHoverSound");
@@ -52,6 +61,7 @@ public class ButtonFeedbackBrain : MonoBehaviour, IPointerEnterHandler, IPointer
     }
     private void PlayPressSound()
     {
+        if (button.interactable == false) return;
         if (button != null)
         {
             AudioManager.Instance.Play("ButtonPressedSound");
@@ -61,15 +71,15 @@ public class ButtonFeedbackBrain : MonoBehaviour, IPointerEnterHandler, IPointer
 
     private void PlayHoverAnimation()
     {
-        button.transform.DOScale(size, animTime).SetEase(ease);
+        button.transform.DOScale(transform.localScale * size, animTime).SetEase(ease);
     }
     private void ReverseHoverAnimation(float animTime)
     {
-        button.transform.DOScale(1f, animTime).SetEase(ease);
+        button.transform.DOScale(initialScale, animTime).SetEase(ease);
     }
     private void PlayPressedAnimation(float animTime)
     {
-        button.transform.DOScale(0.87f, animTime).SetEase(Ease.InOutBack);
+        button.transform.DOScale(initialScale * 0.87f, animTime).SetEase(Ease.InOutBack);
     }
     private void ClearAnimations()
     {
