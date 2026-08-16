bcd1e3ffb28f7ed7369cbdf1b835c4f9fb0e4abe
Juan-Majork <juani120805@gmail.com>
2026-08-16 15:38:20 -0300

    Fixed-Cant use continue in shop

--- Assets/Scripts/UI/LocomotiveUpgrades/UpgradesFlowCOntroller.cs
+++ Assets/Scripts/UI/LocomotiveUpgrades/UpgradesFlowCOntroller.cs
@@ -1,51 +1,11 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UpgradesFlowController : MonoBehaviour
+public class ExitStorePanelCOntroller : MonoBehaviour
 {
-    [SerializeField] private GameObject continueAndReorder;
-    [SerializeField] private GameObject upgradeGameobject;
-    [SerializeField] private LocomotivesUpgradesButtonController upgrades;
-    [SerializeField] private Button upgradesPanelButton;
-
-    private int currentLevel;
-
-    private void Start()
-    {
-        HideAll();
-
-        var sessionConfigRef = ServiceLocator.Get<SessionConfig>();
-        currentLevel = sessionConfigRef.CurrentLevel;
-
-        if (currentLevel <= 0)
-        {
-            upgradesPanelButton.interactable = false;
-        }
-
-        ShowUpgrades();
-    }
-
-    public void ShowContinueAndReorder()
-    {
-        HideAll();
-        continueAndReorder.SetActive(true);
-    }
-
-    public void ShowUpgrades()
-    {
-        upgradeGameobject.SetActive(true);
-        EventBus.Publish(new OnShowCursorEvent(CursorType.Real));
-        EventBus.Publish(new OnActivateUiEvent(false));
-    }
-
     public void ContinueJourney()
     {
         StoreManager.Instance.ExitStore();
     }
 
-    void HideAll()
-    {
-        continueAndReorder.SetActive(false);
-        upgradeGameobject.SetActive(false);
-    }
 }
